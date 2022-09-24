d09edb2d9fadfa6063f2b78a91bf499613a1e824
alexsommers7 <asommers22@hotmail.com>
2022-09-24 11:01:06 -0700

    convert files to jsx and add prettier config for javascriptreact

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,15 +2,22 @@ import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { Flex, Text, Box } from '@chakra-ui/react';
 import { BorderColor } from '../../utils/color-mode-values/colorModeValues.utils';
 
-const Header = (() => {
-	return (
-		<Box as="header" borderBottom="1px solid transparent" borderColor={BorderColor}>
-			<Flex p="8px 32px" alignItems="center" justifyContent="space-between" maxWidth="1400px" m="auto" fontSize="2xl">
-				<Text>StorePi</Text>
-				<ColorModeSwitcher justifySelf="flex-end" />
-			</Flex>
-		</Box>
-	)
-})
+const Header = () => {
+  return (
+    <Box as="header" borderBottom="1px solid transparent" borderColor={BorderColor}>
+      <Flex
+        p="8px 32px"
+        alignItems="center"
+        justifyContent="space-between"
+        maxWidth="1400px"
+        m="auto"
+        fontSize="2xl"
+      >
+        <Text>StorePi</Text>
+        <ColorModeSwitcher justifySelf="flex-end" />
+      </Flex>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
