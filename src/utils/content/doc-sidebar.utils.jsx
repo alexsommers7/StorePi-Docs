5166c259dca22533c62bb37b10c4dd839487ba9a
alexsommers7 <asommers22@hotmail.com>
2022-09-27 20:43:29 -0700

    rename sidebar content util

---
+++ src/utils/content/doc-sidebar.utils.jsx
@@ -0,0 +1,63 @@
+export const DocSidebarItems = [
+  {
+    label: 'Getting Started',
+    subitems: [{ label: 'Introduction', anchor: 'introduction' }],
+  },
+  {
+    label: 'Products',
+    subitems: [
+      { label: 'Get All Products', anchor: 'get-products' },
+      { label: 'Get Product', anchor: 'get-product' },
+    ],
+  },
+  {
+    label: 'Reviews',
+    subitems: [
+      { label: 'Get All Reviews', anchor: 'get-reviews' },
+      { label: 'Get Review', anchor: 'get-review' },
+    ],
+  },
+  {
+    label: 'Brands',
+    subitems: [
+      { label: 'Get All Brands', anchor: 'get-brands' },
+      { label: 'Get Brand', anchor: 'get-brand' },
+    ],
+  },
+  {
+    label: 'Categories',
+    subitems: [
+      { label: 'Get All Categories', anchor: 'get-categories' },
+      { label: 'Get Category', anchor: 'get-category' },
+    ],
+  },
+  {
+    label: 'Carts',
+    subitems: [
+      { label: 'Get All Carts', anchor: 'get-carts' },
+      { label: 'Get Cart', anchor: 'get-cart' },
+    ],
+  },
+  {
+    label: 'Purchases',
+    subitems: [
+      { label: 'Get All Purchases', anchor: 'get-purchases' },
+      { label: 'Get Purchase', anchor: 'get-purchase' },
+    ],
+  },
+  {
+    label: 'Users',
+    subitems: [
+      { label: 'Get All Users', anchor: 'get-users' },
+      { label: 'Get User', anchor: 'get-user' },
+    ],
+  },
+  {
+    label: 'Authentication',
+    subitems: [
+      { label: 'Sign Up', anchor: 'sign-up' },
+      { label: 'Log In', anchor: 'log-in' },
+      { label: 'Log Out', anchor: 'log-out' },
+    ],
+  },
+];
