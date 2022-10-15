b052925a700103a77fae79bea9dc840ed3560f98
alexsommers7 <asommers22@hotmail.com>
2022-10-14 23:10:13 -0700

    init sample request bodies util file

---
+++ src/utils/content/doc-sample-bodies.utils.jsx
@@ -0,0 +1,10 @@
+export const createNewProductBody = `{
+	// all fields are required
+	"sku": 1234,
+	"name": "...",
+	"category": "6213d49cb7f67e0d64774e92",
+	"brand": "...",
+	"regular_price": 99.95,
+	"sale_price": 79.95,
+	"description": "..."
+}`;
