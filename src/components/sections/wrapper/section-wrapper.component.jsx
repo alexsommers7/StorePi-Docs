c841bd1500ad3775ac1201060da307b1d5d80eaa
alexsommers7 <asommers22@hotmail.com>
2022-10-19 23:07:59 -0700

    general styling updates

--- src/components/sections/wrapper/section-wrapper.component.jsx
+++ src/components/sections/wrapper/section-wrapper.component.jsx
@@ -1,11 +1,9 @@
-import { Box, Divider } from '@chakra-ui/react';
-import { BorderColor } from '../../../utils/color/colorModeValues.utils';
+import { Box } from '@chakra-ui/react';
 
-const SectionWrapper = ({ children, useBottomBorder = true, useBottomMargin = true }) => {
+const SectionWrapper = ({ children, useBottomMargin = true }) => {
   return (
-    <Box as="section" mb={useBottomMargin && 14}>
+    <Box as="section" mb={useBottomMargin && 20}>
       {children}
-      <Divider borderColor={useBottomBorder ? BorderColor : 'transparent'} pt={14} />
     </Box>
   );
 };
