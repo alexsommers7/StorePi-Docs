96c734ddaf70bb12b297e293a99aadb8be6eb257
alexsommers7 <asommers22@hotmail.com>
2022-10-23 16:01:48 -0700

    remove lazy-load dependency

--- src/components/endpoint/endpoint.component.jsx
+++ src/components/endpoint/endpoint.component.jsx
@@ -3,7 +3,6 @@ import { Box, Code, Text } from '@chakra-ui/react';
 import AnchorHeading from '../links/anchor/anchor-heading.component';
 import CollapsibleCode from '../collapsible-code/collapsible-code.component';
 import { apiOrigin } from '../../utils/content/doc-content.utils';
-import LazyLoad from 'react-lazy-load';
 import Prism from 'prismjs';
 
 const Endpoint = ({
@@ -23,39 +22,37 @@ const Endpoint = ({
   const isDelete = httpMethod === 'DELETE';
 
   return (
-    <LazyLoad>
-      <Box mt={14} mb={isDelete ? 6 : 20}>
-        <AnchorHeading anchorId={anchorId} httpMethod={httpMethod} requiresAuth={requiresAuth}>
-          {headingText}
-        </AnchorHeading>
-        <Box mb={7}>
-          <Box overflowX="auto">
-            <Code mt={3} mb={5}>
-              {apiOrigin}/{subdirectory}
-            </Code>
-          </Box>
-
-          {note && <Text mb={4}>{note}</Text>}
-
-          {requestBody && (
-            <Box mb={5}>
-              <Text mb={1}>Sample request body:</Text>
-              <pre>
-                <Code className="language-javascript">{requestBody}</Code>
-              </pre>
-            </Box>
-          )}
-
-          {isDelete && (
-            <Text>
-              Returns <Code>204 No Content</Code>
-            </Text>
-          )}
-
-          {response && <CollapsibleCode code={response} />}
+    <Box mt={14} mb={isDelete ? 6 : 20}>
+      <AnchorHeading anchorId={anchorId} httpMethod={httpMethod} requiresAuth={requiresAuth}>
+        {headingText}
+      </AnchorHeading>
+      <Box mb={7}>
+        <Box overflowX="auto">
+          <Code mt={3} mb={5}>
+            {apiOrigin}/{subdirectory}
+          </Code>
         </Box>
+
+        {note && <Text mb={4}>{note}</Text>}
+
+        {requestBody && (
+          <Box mb={5}>
+            <Text mb={1}>Sample request body:</Text>
+            <pre>
+              <Code className="language-javascript">{requestBody}</Code>
+            </pre>
+          </Box>
+        )}
+
+        {isDelete && (
+          <Text>
+            Returns <Code>204 No Content</Code>
+          </Text>
+        )}
+
+        {response && <CollapsibleCode code={response} />}
       </Box>
-    </LazyLoad>
+    </Box>
   );
 };
 
