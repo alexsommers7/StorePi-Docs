1a9d736e3d08667602f6308a2f34dcfd5421d9f1
alexsommers7 <asommers22@hotmail.com>
2022-12-23 19:13:03 -0700

    Implement sidebar live scroll functionality

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsSidebarOpen } from '../../store/docs/docs.selector';
 import { Flex, UnorderedList, Box, useMediaQuery } from '@chakra-ui/react';
-
+import { selectActiveAnchor } from '../../store/docs/docs.selector';
 import { BorderColor } from '../../utils/color/colorModeValues.utils';
 import { DocSidebarItems } from '../../utils/content/doc-sidebar.utils';
 import SidebarLink from './link/sidebar-link.component';
@@ -11,12 +12,25 @@ import { navHeight } from '../../utils/sizing/sizing.utils';
 import { contentMaxWidth } from '../../utils/sizing/sizing.utils';
 
 const openStyle = {
-	transform: 'translateY(-1.25rem)',
+  transform: 'translateY(-1.25rem)',
 };
+
+const scrollToSidebarSection = hash =>
+  document
+    .querySelector(`[data-href="#${hash}"]`)
+    ?.closest('ul')
+    ?.scrollIntoView({ block: 'start' });
 
 const Sidebar = () => {
   const [isLargerThanMaxContent] = useMediaQuery(`(min-width: ${contentMaxWidth.num + 55}px)`);
   const isSidebarOpen = useSelector(selectIsSidebarOpen);
+  const activeAnchor = useSelector(selectActiveAnchor);
+  const [previousAnchor, setPreviousAnchor] = useState(activeAnchor);
+
+  if (activeAnchor !== previousAnchor) {
+    setPreviousAnchor(activeAnchor);
+    scrollToSidebarSection(activeAnchor);
+  }
 
   // using transform instead of top/left so it remains fixed to container, not viewport
   return (
@@ -54,6 +68,7 @@ const Sidebar = () => {
                 style={{ marginInlineStart: '2px' }}
                 key={label}
                 mb={7}
+                scrollMargin="10px"
               >
                 <SidebarHeading label={label} isFirst={i === 0} />
                 {subitems?.length &&
